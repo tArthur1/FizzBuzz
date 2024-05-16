d3c67beb0bf5711d5ccb448696f9435d89b7c85a
Arthur <artam9310@gmail.com>
2024-05-16 19:29:58 -0400

    ADD: multiples and reduce methods

---
+++ src/Reduce.java
@@ -0,0 +1,15 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int steps = 0;
+        while (i > 0){
+            if (i%2==0){
+                i = i / 2;
+            } else {
+                i--;
+            }
+            steps++;
+        }
+        System.out.println(steps);
+    }
+}
